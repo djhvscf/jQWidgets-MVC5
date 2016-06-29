f26780f027bfe29b3e893f956e583e6e9c480c67
Dennis Hernández <dennishernandezvargas@gmail.com>
2016-06-28 21:53:25 -0600

    Adding the libs

---
+++ template-jqwidgets-mvc5/template-jqwidgets-mvc5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="template_jqwidgets_mvc5.MvcApplication" Language="C#" %>
---
+++ template-jqwidgets-mvc5/template-jqwidgets-mvc5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(template_jqwidgets_mvc5.Startup))]
+namespace template_jqwidgets_mvc5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
